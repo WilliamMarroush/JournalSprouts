890ec4f45d217ae27e036277118c9bd8c9f3f008
William Marroush <William.Marroush@csi.cuny.edu>
2025-10-06 17:59:45 -0400

    Erasing old function

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-function newEntry() {
-    //Create a li element
-    var listelement = document.createElement("li");
-    //Get the text-box user input
-    var inputValue = document.getElementById("EntryContent").value;
-    //create and attatch the text element to the list element
-    var textval = document.createTextNode(inputValue);
-    listelement.appendChild(textval);
-
-
-    if (inputValue == ""){
-        alert("You gotta journal!");
-        //If the user just hits the button
-    }
-    else {
-        document.getElementById("garden").appendChild(listelement);
-        //Add the user's input to the garden!
-    }
-    //clear the textbox
-    document.getElementById("EntryContent").value = "";
-}
-
 function newEntry(){
     //Create the Journal Entry elements
     var entryli = document.createElement("li");
