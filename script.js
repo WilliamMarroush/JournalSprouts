6f05e934938aeb12872b454485114386a9cab7a4
William <marroushw@yahoo.com>
2025-10-23 10:31:59 -0400

    comments to easier define function uses

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let streak=0;
 let elEntryTitle,elEntryContent,elJournal,elJournalDiv,elGardenDiv;
 
 /*#####INITIALIZATION#####*/
+//This is gonna run as soon as the page starts,
+//anything you want done on startup should go here
 document.addEventListener("DOMContentLoaded",function(){
     elEntryTitle = document.getElementById("EntryName");
     elEntryContent = document.getElementById("EntryContent");
@@ -22,6 +24,9 @@ document.addEventListener("DOMContentLoaded",function(){
 });
 
 /*#####DATA FUNCTIONS#####*/
+//Input for createJournalEntry (Technically): enTitle, enContent
+//Output for createJournalEntry: New JournalEntry Object, pushed onto 
+//journalEntries array
 function createJournalEntry(){
     var enTitle = elEntryTitle.value;
     var enContent = elEntryContent.value;
@@ -39,6 +44,9 @@ function createJournalEntry(){
 }
 
 /*#####RENDER FUNCTIONS#####*/
+//Input for renderJournal: entirety of journalEntry array
+//output for renderJournal: HTML dom output, giving each journal entry
+//it's own little card.
 function renderJournal(){
     //clearing garden elements before displaying (to avoid repeating entries)
     elJournal.replaceChildren();
@@ -61,12 +69,16 @@ function renderJournal(){
     elEntryTitle.value = "";
     elEntryContent.value = "";
 }
+//Input for updateEntryCount: journalEntries array
+//output for updateEntryCount: updating the entry count variable
 function updateEntryCount(){
     var entCount = document.getElementById("entryCount");
     entCount.innerHTML = `
     <p>Journal Entries: ${journalEntries.length}</p>
     `;
 }
+//Input for toggleView: which view to toggle to
+//output for toggleView: Change which div has "hidden" class
 function toggleView(option){
     if (option == 'journal'){
         elJournalDiv.classList.remove("hidden");
@@ -78,9 +90,15 @@ function toggleView(option){
     }
 }
 /*#####STORAGE FUNCTIONS#####*/
+//Input for saveJournal: entire journalEntries array
+//output for saveJournal: None technically // pushing JSON string of
+//journalEntries array to localStorage
 function saveJournal(){
     localStorage.setItem("Journal", JSON.stringify(journalEntries));
 }
+//Input for loadSavedJournal: JSON for "Journal" from localStorage
+//output for loadSavedJournal: updating journalEntries array to have
+//all current and previously saved entries
 function loadSavedJournal(){
     let tempJournal = JSON.parse(localStorage.getItem("Journal"));
     if (tempJournal && Array.isArray(tempJournal)){
@@ -94,6 +112,10 @@ function loadSavedJournal(){
 }
 
 /*#####EVENT HANDLER FUNCTIONS#####*/
+//input for clearJournal: confirmation (Do you really want to delete your journal?)
+//output for clearJournal: 
+//                         if yes: clears journalEntries array, and clears saved journalEntries array
+//                         if no: nothing
 function clearJournal(){
     if (confirm("Are you sure?")){
         journalEntries = [];
@@ -106,6 +128,9 @@ function clearJournal(){
         alert("Journal was not deleted.");
     }  
 }
+//input for addNewEntry: Title and Content of input textboxes
+//output for addNewEntry: creates new JournalEntry object, and push it onto journalList. Save journal
+//in localStorage
 function addNewEntry(){
     createJournalEntry();
     renderJournal();
@@ -114,6 +139,9 @@ function addNewEntry(){
     calculateStreak();
     elEntryTitle.focus();
 }
+//Input for calculateStreak: Entire journalEntries array
+//output for calculateStreak: single number, streak. Number of sequential days in a row a journal
+//entry has been made
 function calculateStreak(){
     //extract all unique date values from journalEntries array
     let datestamps = journalEntries.map(entry => entry.date);
